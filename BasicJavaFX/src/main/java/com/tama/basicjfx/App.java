b405532fdb5a0acfeb984f107104b3eba2476163
raden rangga pratama <daris.rangga.pratama@gmail.com>
2025-06-21 14:48:55 +0700

    initial commit

--- BasicJavaFX/src/main/java/com/tama/basicjfx/App.java
+++ BasicJavaFX/src/main/java/com/tama/basicjfx/App.java
@@ -12,7 +12,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("app-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Trial JavaFX Application");
+        stage.setTitle("Trial JavaFX App");
         stage.setScene(scene);
         stage.show();
     }
